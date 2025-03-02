60ff891171920748193bc1be69df29f71b80f86d
Alansmatias <alanmatyas5@gmail.com>
2025-03-01 23:05:32 -0300

    banco de dados

--- NO-LARAVEL/Cadastro de Setores e Turnos.sql
+++ NO-LARAVEL/Cadastro de Setores e Turnos.sql
@@ -1,25 +1,26 @@
 INSERT INTO `laravel`.`setors` (`nome`, `ativo`) 
 VALUES
+('Almoxarifado', '1'),
+('Assadeiras', '1'),
 ('Atendimento', '1'),
 ('Balcão', '1'),
 ('Caixa', '1'),
-('Lanchonete', '1'),
-('Restaurante', '1'),
-('Supervisor Buffet', '1'),
-('Almoxarifado', '1'),
 ('Confeitaria', '1'),
-('Padaria', '1'),
-('Forno', '1'),
-('Assadeiras', '1'),
+('Cozinha', '1'),
 ('Embalagem', '1'),
 ('Entrega', '1'),
-('Venda Externa', '1'),
-('Rota', '1'),
-('Monitoria', '1'),
-('Cozinha', '1'),
-('Serviços Gerais', '1'),
 ('Escritório', '1'),
-('Padaria Auxiliar', '1');
+('Forno', '1'),
+('Lanchonete', '1'),
+('Monitoria', '1'),
+('Padaria', '1'),
+('Padaria Auxiliar', '1'),
+('Reposição', '1'),
+('Restaurante', '1'),
+('Rota', '1'),
+('Serviços Gerais', '1'),
+('Supervisor Buffet', '1'),
+('Venda Externa', '1');
 
 INSERT INTO `laravel`.`turnos` (`nome`, `ativo`) 
 VALUES
